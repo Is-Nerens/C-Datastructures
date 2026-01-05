c58e8a79a2582e9c6757ca5b397620828b768255
Arran Stevens <55632520+Is-Nerens@users.noreply.github.com>
2026-01-05 11:35:20 +0200

    Update String.h

--- String.h
+++ String.h
@@ -9,42 +9,43 @@ typedef char* String;
 
 inline char* StringCstr(String str)
 {
-    return str + 5;
+    return str + 4;
+}
+
+inline uint32_t StringLen(String string)
+{
+    return ((uint32_t)(unsigned char)string[0] << 16) |
+           ((uint32_t)(unsigned char)string[1] << 8) |
+           ((uint32_t)(unsigned char)string[2]);
 }
 
 String StringCreate(char* str)
 {
     uint32_t length = strlen(str);
-    String result = malloc(length + 6);
+    String result = malloc(length + 4 + 1);
     if (!result) return NULL;
-    memcpy(result + 5, str, length);
-    memcpy(result, &length, 4);
-    result[4] = ASCII;
-    result[length + 5] = '\0';
+    memcpy(result + 4, str, length);
+    // write metadata into first 4 bytes [length 3][encoding 1]
+    result[0] = (length >> 16) & 0xFF;
+    result[1] = (length >> 8) & 0xFF;
+    result[2] = length & 0xFF;  
+    result[3] = ASCII;
+    result[length + 4] = '\0';
     return result;
 }
 
 String StringCreateUTF8(char* str)
 {
     uint32_t length = strlen(str);
-    String result = malloc(length + 6);
+    String result = malloc(length + 4 + 1);
     if (!result) return NULL;
-    memcpy(result + 5, str, length);
-    memcpy(result, &length, 4);
-    result[4] = UTF8;
-    result[length + 5] = '\0';
-    return result;
-}
-
-String StringCreateUTF32(char* str)
-{
-    uint32_t length = strlen(str);
-    String result = malloc(length + 6);
-    if (!result) return NULL;
-    memcpy(result + 5, str, length);
-    memcpy(result, &length, 4);
-    result[4] = UTF32;
-    result[length + 5] = '\0';
+    memcpy(result + 4, str, length);
+    // write metadata into first 4 bytes [length 3][encoding 1]
+    result[0] = (length >> 16) & 0xFF;
+    result[1] = (length >> 8) & 0xFF;
+    result[2] = length & 0xFF;  
+    result[3] = UTF8;
+    result[length + 4] = '\0';
     return result;
 }
 
@@ -59,7 +60,7 @@ inline void StringFree(String* str)
 uint32_t GetNextUTF8Codepoint(String string, int* i)
 {
     char* cstr = StringCstr(string);
-    uint32_t stringLen = *(uint32_t*)string;
+    uint32_t stringLen = StringLen(string);
     uint32_t codepoint = 0;
     unsigned char firstByte = (unsigned char)cstr[*i];
 
@@ -88,30 +89,39 @@ uint32_t GetNextUTF8Codepoint(String string, int* i)
     return codepoint;
 } 
 
+inline uint32_t GetUTF32Codepoint(String string, int i) 
+{
+    uint32_t codepoint;
+    memcpy(&codepoint, StringCstr(string) + i * 4, 4);
+    return codepoint;
+}
+
 String StringConcat(String a, String b)
 {
     // validate strings and get metadata
     if (a == NULL || b == NULL) return NULL;
-    uint32_t aLen = *(uint32_t*)a;
-    uint32_t bLen = *(uint32_t*)b;
-    char aEncoding = a[4];
-    char bEncoding = b[4];
+    uint32_t aLen = StringLen(a);
+    uint32_t bLen = StringLen(b);
+    char aEncoding = a[3];
+    char bEncoding = b[3];
     if (aEncoding != bEncoding || aLen == 0 || bLen == 0) return NULL;
 
     // allocate space for return string
     String output = NULL;
-    output = malloc(aLen + bLen + 6);
+    output = malloc(aLen + bLen + 4 + 1);
     if (output == NULL) return NULL;
 
     // copy data from string a and b
-    memcpy(output + 5, a + 5, aLen);
-    memcpy(output + 5 + aLen, b + 5, bLen);
+    memcpy(output + 4, a + 4, aLen);
+    memcpy(output + 4 + aLen, b + 4, bLen);
 
     // set metadata
     uint32_t outputLen = aLen + bLen;
-    memcpy(output, &outputLen, 4);
-    output[4] = aEncoding;
-    output[outputLen + 5] = '\0';
+    output[0] = (outputLen >> 16) & 0xFF;
+    output[1] = (outputLen >> 8) & 0xFF;
+    output[2] = outputLen & 0xFF;  
+    output[3] = aEncoding;
+    output[outputLen + 4] = '\0';
     return output;
 }
 
@@ -119,22 +129,24 @@ int StringContains(String string, String search)
 {
     // validate strings and get metadata
     if (string == NULL || search == NULL) return 0;
-    uint32_t stringLen = *(uint32_t*)string;
-    uint32_t searchLen = *(uint32_t*)search;
-    char stringEncoding = string[4];
-    char searchEncoding = search[4];
+    uint32_t stringLen = StringLen(string);
+    uint32_t searchLen = StringLen(search);
+    char stringEncoding = string[3];
+    char searchEncoding = search[3];
     if (stringEncoding != searchEncoding || stringLen == 0 || searchLen == 0) {
         return 0;
     }
+    char* stringCstr = StringCstr(string);
+    char* searchCstr = StringCstr(search);
 
-    if (stringEncoding == ASCII)
+    if (stringEncoding == ASCII || stringEncoding == UTF8)
     {
         // longest prefix-suffix
-        uint8_t* lps = (uint8_t*)calloc(searchLen, 1);
+        uint16_t* lps = (uint16_t*)calloc(searchLen, sizeof(uint16_t));
         int j = 0;
         int i = 1;
         while (i < searchLen) {
-            if (search[i + 5] == search[j + 5]) {
+            if (stringCstr[i] == searchCstr[j]) {
                 j += 1; lps[i] = j; i += 1;
             }
             else {
@@ -149,28 +161,31 @@ int StringContains(String string, String search)
         i = 0;
         j = 0;
         while (i < stringLen) {
-            if (string[i + 5] == search[j + 5]) {
+            if (stringCstr[i] == searchCstr[j]) {
                 i++; j++;
             }
             if (j == searchLen) {
                 free(lps);
                 return 1;
             }
-            else if (i < stringLen && string[i + 5] != search[j + 5]) {
+            else if (i < stringLen && stringCstr[i] != searchCstr[j]) {
                 if (j != 0) { j = lps[j-1]; } 
                 else { i++; }
             }
         }
         free(lps);
     }
-    else if (stringEncoding == UTF8)
+    else if (stringEncoding == UTF32)
     {
+        uint32_t stringCount = stringLen / 4;
+        uint32_t searchCount = searchLen / 4;
+
         // longest prefix-suffix
-        uint8_t* lps = (uint8_t*)calloc(searchLen, 1);
+        uint32_t* lps = (uint32_t*)calloc(searchCount, sizeof(uint32_t));
         int j = 0;
         int i = 1;
-        while (i < searchLen) {
-            if (search[i + 5] == search[j + 5]) {
+        while (i < searchCount) {
+            if (GetUTF32Codepoint(search, i) == GetUTF32Codepoint(search, j)) {
                 j += 1; lps[i] = j; i += 1;
             }
             else {
@@ -184,22 +199,21 @@ int StringContains(String string, String search)
         // knuth-morris-pratt
         i = 0;
         j = 0;
-        while (i < stringLen) {
-            if (string[i + 5] == search[j + 5]) {
+        while (i < stringCount) {
+            if (GetUTF32Codepoint(string, i) == GetUTF32Codepoint(search, j)) {
                 i++; j++;
             }
-            if (j == searchLen) {
+            if (j == searchCount) {
                 free(lps);
                 return 1;
             }
-            else if (i < stringLen && string[i + 5] != search[j + 5]) {
+            else if (i < stringCount && GetUTF32Codepoint(string, i) != GetUTF32Codepoint(search, j)) {
                 if (j != 0) { j = lps[j-1]; } 
                 else { i++; }
             }
         }
         free(lps);
     }
-
     return 0;
 }
 
@@ -207,20 +221,20 @@ int StringEquals(String a, String b)
 {
     if (!a || !b) return 0;
     uint32_t aLen = *(uint32_t*)a;
-    return memcmp(a, b, aLen + 5) == 0;
+    return memcmp(a, b, aLen + 4) == 0;
 }
 
 void StringEncodeUTF8(String* string)
 {
     // validate string and get metadata
     if (string == NULL || *string == NULL) return;
-    uint32_t stringLen = *(uint32_t*)*string;
-    char stringEncoding = (*string)[4];
+    uint32_t stringLen = StringLen(*string);
+    char stringEncoding = (*string)[3];
     if (stringLen == 0 || stringEncoding == UTF8) return;
 
     if (stringEncoding == ASCII)
     {
-        (*string)[4] = UTF8;
+        (*string)[3] = UTF8;
         return;
     }
     else if (stringEncoding == UTF32)
@@ -243,8 +257,10 @@ void StringEncodeUTF8(String* string)
         if (encoded == NULL) return;
 
         // set metadata
-        memcpy(encoded, &bufferSize, 4);
-        encoded[4] = UTF8;
+        encoded[0] = (bufferSize >> 16) & 0xFF;
+        encoded[1] = (bufferSize >> 8) & 0xFF;
+        encoded[2] = bufferSize & 0xFF;  
+        encoded[3] = UTF8;
         encodedCstr[bufferSize] = '\0';
 
         // encode
@@ -283,8 +299,8 @@ void StringEncodeUTF32(String* string)
 {
     // validate string and get metadata
     if (string == NULL) return;
-    uint32_t stringLen = *(uint32_t*)*string;
-    char stringEncoding = (*string)[4];
+    uint32_t stringLen = StringLen(*string);
+    char stringEncoding = (*string)[3];
     if (stringLen == 0 || stringEncoding == UTF32) return;
     char* cstr = StringCstr(*string);
 
@@ -292,14 +308,15 @@ void StringEncodeUTF32(String* string)
     {
         // allocate space for encoded result
         uint32_t encodedLen = stringLen * 4;
-        String encoded = malloc(encodedLen + 6);
+        String encoded = malloc(encodedLen + 4);
         char* encodedCstr = StringCstr(encoded);
         if (encoded == NULL) return;
 
         // set encoded metadata
-        memcpy(encoded, &encodedLen, 4);
-        encoded[4] = UTF32;
-        encodedCstr[stringLen * 4] = '\0';
+        encoded[0] = (encodedLen >> 16) & 0xFF;
+        encoded[1] = (encodedLen >> 8) & 0xFF;
+        encoded[2] = encodedLen & 0xFF;  
+        encoded[3] = UTF32;
         
         // encode
         for (uint32_t i=0; i<stringLen; i++)
@@ -317,7 +334,7 @@ void StringEncodeUTF32(String* string)
     if (stringEncoding == UTF8)
     {
         // over-allocate space for encoded result
-        String encoded = malloc(stringLen * 4 + 6);
+        String encoded = malloc(stringLen * 4 + 4);
         if (encoded == NULL) return;
 
         // encode
@@ -325,19 +342,20 @@ void StringEncodeUTF32(String* string)
         int i = 0;
         while (i < stringLen) {
             uint32_t codepoint = GetNextUTF8Codepoint(*string, &i);
-            memcpy(encoded + 5 + utf32Len, &codepoint, 4);
+            memcpy(encoded + 4 + utf32Len, &codepoint, 4);
             utf32Len += 4;
         }
 
         // shrink over-allocated buffer
         if (utf32Len > stringLen * 4) {
-            encoded = realloc(encoded, utf32Len + 6);
+            encoded = realloc(encoded, utf32Len + 4);
         }
 
         // set metadata
-        memcpy(encoded, &utf32Len, 4);
-        encoded[4] = UTF32;
-        encoded[utf32Len + 5] = '\0';
+        encoded[0] = (utf32Len >> 16) & 0xFF;
+        encoded[1] = (utf32Len >> 8) & 0xFF;
+        encoded[2] = utf32Len & 0xFF;  
+        encoded[3] = UTF32;
 
         // update string
         free(*string);
